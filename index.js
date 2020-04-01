2e6e2a4cb5b80210f54b978b9c8eef5480a5e824
DominikKraus <dominik04kraus@gmail.com>
2020-04-01 23:20:23 +0200

    added verify route

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const port = 1010;
+const mongoose = require('mongoose');
 
 dotenv.config();
+
+mongoose.connect(
+    process.env.DB_KEY,
+    () => console.log('Database connection successful.')
+);
+
+const login = require('./routes/login');
 
 app.listen(port, () => console.log(`Server started on port ${port}!`));
